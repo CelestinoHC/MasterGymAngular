72e9e37606d6eb55ef0531c7d768083d15004d24
CelestinoHC <32680433+CelestinoHC@users.noreply.github.com>
2022-01-04 17:08:57 -0600

    Maquetar Formulario Agregar Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,16 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-
+import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     EncabezadoComponent,
-    ListadoClientesComponent
+    ListadoClientesComponent,
+    AgregarClienteComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +33,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
     BrowserAnimationsModule,    
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
+    ProgressbarModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     ReactiveFormsModule,
     FormsModule,
