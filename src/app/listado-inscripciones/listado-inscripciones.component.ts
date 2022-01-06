21ca1138caa49ee5041223f03cdbd00e61473c20
CelestinoHC <32680433+CelestinoHC@users.noreply.github.com>
2022-01-05 23:08:45 -0600

    Mostrar inscripciones

---
+++ src/app/listado-inscripciones/listado-inscripciones.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Inscripcion } from '../models/inscripcion';
+
+@Component({
+  selector: 'app-listado-inscripciones',
+  templateUrl: './listado-inscripciones.component.html',
+  styleUrls: ['./listado-inscripciones.component.scss']
+})
+export class ListadoInscripcionesComponent implements OnInit {
+  inscripciones: any[] = new Array<any>();
+  constructor(private db: AngularFirestore) { }
+
+  ngOnInit(): void {
+    this.inscripciones.length = 0;
+    this.db.collection('inscripciones').get().subscribe((resultado)=>{
+      resultado.forEach((inscripcion)=>{
+        let inscripcionObtenida:any = inscripcion.data();
+        inscripcionObtenida.id = inscripcion.id;
+        let url = inscripcionObtenida.cliente.path;
+        
+        this.db.doc(url).get().subscribe((cliente)=>{
+          inscripcionObtenida.clienteObtenido = cliente.data();
+          inscripcionObtenida.fecha = new Date(inscripcionObtenida.fecha.seconds * 1000);
+          inscripcionObtenida.fechaFinal = new Date(inscripcionObtenida.fechaFinal.seconds *1000);
+          
+          this.inscripciones.push(inscripcionObtenida);
+        })
+      })
+    })
+
+  }
+
+}
